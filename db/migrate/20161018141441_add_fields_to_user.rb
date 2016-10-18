6b7c8837c7447123fdcf569ba6b0f8a1d76c087f
Shelby Solomon <shelby@shelbysolomon.net>
2016-10-18 09:48:31 -0500

    added fields to user model

---
+++ db/migrate/20161018141441_add_fields_to_user.rb
@@ -0,0 +1,8 @@
+class AddFieldsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :role, :string
+    add_column :users, :permission, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018005119) do
+ActiveRecord::Schema.define(version: 20161018141441) do
 
   create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20161018005119) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "company_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "role"
+    t.integer  "permission"
     t.index ["company_id"], name: "index_users_on_company_id", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
