81fd7de13ab558e4541c0118e65a6784636e42e8
Shelby Solomon <shelby@shelbysolomon.net>
2016-10-19 20:44:09 -0500

    sidebar menu created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'dashboards#index'
+
+  devise_scope :user do
+    resources :users
+  end
+
+  resources :companies
+
+  resources :productions
 end
